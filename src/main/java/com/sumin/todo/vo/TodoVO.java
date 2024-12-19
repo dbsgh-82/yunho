e95da78d3653b6f66078d18ecf8533a41232bfff
unknown <dbsgh8242@gmail.com>
2024-12-19 21:38:37 +0900

    initial

---
+++ src/main/java/com/sumin/todo/vo/TodoVO.java
@@ -0,0 +1,13 @@
+package com.sumin.todo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class TodoVO {
+	private int todoId;
+	private String todoTitle;
+}
